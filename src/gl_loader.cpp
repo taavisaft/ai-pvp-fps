2957129a12fa4347cb7e41c67d66e31f0cb9912b
Taavi Saft <taavi@bonefarm.ee>
2026-06-16 10:59:29 +0300

    Refactor OpenGL function loading to support symbol renaming, enhancing flexibility in function management.

--- src/gl_loader.cpp
+++ src/gl_loader.cpp
@@ -4,15 +4,15 @@
 #include <SDL.h>
 #include <cstdio>
 
-#define GL_DEFINE(type, name) type name = nullptr;
+#define GL_DEFINE(type, name, symbol) type name = nullptr;
 GL_CORE_FUNCS(GL_DEFINE)
 #undef GL_DEFINE
 
 bool loadGLFunctions() {
     bool ok = true;
-    #define GL_LOAD_FUNC(type, name) \
-        name = (type)SDL_GL_GetProcAddress(#name); \
-        if (!name) { fprintf(stderr, "GL load failed: %s\n", #name); ok = false; }
+    #define GL_LOAD_FUNC(type, name, symbol) \
+        name = (type)SDL_GL_GetProcAddress(#symbol); \
+        if (!name) { fprintf(stderr, "GL load failed: %s\n", #symbol); ok = false; }
     GL_CORE_FUNCS(GL_LOAD_FUNC)
     #undef GL_LOAD_FUNC
     return ok;
--- src/gl_loader.h
+++ src/gl_loader.h
@@ -12,40 +12,69 @@
   #include <SDL_opengl.h>
 
   #define GL_CORE_FUNCS(X) \
-      X(PFNGLCREATESHADERPROC,            glCreateShader) \
-      X(PFNGLSHADERSOURCEPROC,            glShaderSource) \
-      X(PFNGLCOMPILESHADERPROC,           glCompileShader) \
-      X(PFNGLGETSHADERIVPROC,             glGetShaderiv) \
-      X(PFNGLGETSHADERINFOLOGPROC,        glGetShaderInfoLog) \
-      X(PFNGLDELETESHADERPROC,            glDeleteShader) \
-      X(PFNGLCREATEPROGRAMPROC,           glCreateProgram) \
-      X(PFNGLATTACHSHADERPROC,            glAttachShader) \
-      X(PFNGLLINKPROGRAMPROC,             glLinkProgram) \
-      X(PFNGLGETPROGRAMIVPROC,            glGetProgramiv) \
-      X(PFNGLGETPROGRAMINFOLOGPROC,       glGetProgramInfoLog) \
-      X(PFNGLDELETEPROGRAMPROC,           glDeleteProgram) \
-      X(PFNGLUSEPROGRAMPROC,              glUseProgram) \
-      X(PFNGLGETUNIFORMLOCATIONPROC,      glGetUniformLocation) \
-      X(PFNGLUNIFORMMATRIX4FVPROC,        glUniformMatrix4fv) \
-      X(PFNGLUNIFORM1FPROC,               glUniform1f) \
-      X(PFNGLUNIFORM3FPROC,               glUniform3f) \
-      X(PFNGLUNIFORM1FPROC,               glUniform1f) \
-      X(PFNGLUNIFORM1IPROC,               glUniform1i) \
-      X(PFNGLACTIVETEXTUREPROC,           glActiveTexture) \
-      X(PFNGLBUFFERSUBDATAPROC,           glBufferSubData) \
-      X(PFNGLGENVERTEXARRAYSPROC,         glGenVertexArrays) \
-      X(PFNGLBINDVERTEXARRAYPROC,         glBindVertexArray) \
-      X(PFNGLDELETEVERTEXARRAYSPROC,      glDeleteVertexArrays) \
-      X(PFNGLGENBUFFERSPROC,              glGenBuffers) \
-      X(PFNGLBINDBUFFERPROC,              glBindBuffer) \
-      X(PFNGLBUFFERDATAPROC,              glBufferData) \
-      X(PFNGLDELETEBUFFERSPROC,           glDeleteBuffers) \
-      X(PFNGLENABLEVERTEXATTRIBARRAYPROC, glEnableVertexAttribArray) \
-      X(PFNGLVERTEXATTRIBPOINTERPROC,     glVertexAttribPointer)
+      X(PFNGLCREATESHADERPROC,            fps_glCreateShader,            glCreateShader) \
+      X(PFNGLSHADERSOURCEPROC,            fps_glShaderSource,            glShaderSource) \
+      X(PFNGLCOMPILESHADERPROC,           fps_glCompileShader,           glCompileShader) \
+      X(PFNGLGETSHADERIVPROC,             fps_glGetShaderiv,             glGetShaderiv) \
+      X(PFNGLGETSHADERINFOLOGPROC,        fps_glGetShaderInfoLog,        glGetShaderInfoLog) \
+      X(PFNGLDELETESHADERPROC,            fps_glDeleteShader,            glDeleteShader) \
+      X(PFNGLCREATEPROGRAMPROC,           fps_glCreateProgram,           glCreateProgram) \
+      X(PFNGLATTACHSHADERPROC,            fps_glAttachShader,            glAttachShader) \
+      X(PFNGLLINKPROGRAMPROC,             fps_glLinkProgram,             glLinkProgram) \
+      X(PFNGLGETPROGRAMIVPROC,            fps_glGetProgramiv,            glGetProgramiv) \
+      X(PFNGLGETPROGRAMINFOLOGPROC,       fps_glGetProgramInfoLog,       glGetProgramInfoLog) \
+      X(PFNGLDELETEPROGRAMPROC,           fps_glDeleteProgram,           glDeleteProgram) \
+      X(PFNGLUSEPROGRAMPROC,              fps_glUseProgram,              glUseProgram) \
+      X(PFNGLGETUNIFORMLOCATIONPROC,      fps_glGetUniformLocation,      glGetUniformLocation) \
+      X(PFNGLUNIFORMMATRIX4FVPROC,        fps_glUniformMatrix4fv,        glUniformMatrix4fv) \
+      X(PFNGLUNIFORM1FPROC,               fps_glUniform1f,               glUniform1f) \
+      X(PFNGLUNIFORM3FPROC,               fps_glUniform3f,               glUniform3f) \
+      X(PFNGLUNIFORM1IPROC,               fps_glUniform1i,               glUniform1i) \
+      X(PFNGLACTIVETEXTUREPROC,           fps_glActiveTexture,           glActiveTexture) \
+      X(PFNGLBUFFERSUBDATAPROC,           fps_glBufferSubData,           glBufferSubData) \
+      X(PFNGLGENVERTEXARRAYSPROC,         fps_glGenVertexArrays,         glGenVertexArrays) \
+      X(PFNGLBINDVERTEXARRAYPROC,         fps_glBindVertexArray,         glBindVertexArray) \
+      X(PFNGLDELETEVERTEXARRAYSPROC,      fps_glDeleteVertexArrays,      glDeleteVertexArrays) \
+      X(PFNGLGENBUFFERSPROC,              fps_glGenBuffers,              glGenBuffers) \
+      X(PFNGLBINDBUFFERPROC,              fps_glBindBuffer,              glBindBuffer) \
+      X(PFNGLBUFFERDATAPROC,              fps_glBufferData,              glBufferData) \
+      X(PFNGLDELETEBUFFERSPROC,           fps_glDeleteBuffers,           glDeleteBuffers) \
+      X(PFNGLENABLEVERTEXATTRIBARRAYPROC, fps_glEnableVertexAttribArray, glEnableVertexAttribArray) \
+      X(PFNGLVERTEXATTRIBPOINTERPROC,     fps_glVertexAttribPointer,     glVertexAttribPointer)
 
-  #define GL_DECLARE(type, name) extern type name;
+  #define GL_DECLARE(type, name, symbol) extern type name;
   GL_CORE_FUNCS(GL_DECLARE)
   #undef GL_DECLARE
+
+  #define glCreateShader            fps_glCreateShader
+  #define glShaderSource            fps_glShaderSource
+  #define glCompileShader           fps_glCompileShader
+  #define glGetShaderiv             fps_glGetShaderiv
+  #define glGetShaderInfoLog        fps_glGetShaderInfoLog
+  #define glDeleteShader            fps_glDeleteShader
+  #define glCreateProgram           fps_glCreateProgram
+  #define glAttachShader            fps_glAttachShader
+  #define glLinkProgram             fps_glLinkProgram
+  #define glGetProgramiv            fps_glGetProgramiv
+  #define glGetProgramInfoLog       fps_glGetProgramInfoLog
+  #define glDeleteProgram           fps_glDeleteProgram
+  #define glUseProgram              fps_glUseProgram
+  #define glGetUniformLocation      fps_glGetUniformLocation
+  #define glUniformMatrix4fv        fps_glUniformMatrix4fv
+  #define glUniform1f               fps_glUniform1f
+  #define glUniform3f               fps_glUniform3f
+  #define glUniform1i               fps_glUniform1i
+  #define glActiveTexture           fps_glActiveTexture
+  #define glBufferSubData           fps_glBufferSubData
+  #define glGenVertexArrays         fps_glGenVertexArrays
+  #define glBindVertexArray         fps_glBindVertexArray
+  #define glDeleteVertexArrays      fps_glDeleteVertexArrays
+  #define glGenBuffers              fps_glGenBuffers
+  #define glBindBuffer              fps_glBindBuffer
+  #define glBufferData              fps_glBufferData
+  #define glDeleteBuffers           fps_glDeleteBuffers
+  #define glEnableVertexAttribArray fps_glEnableVertexAttribArray
+  #define glVertexAttribPointer     fps_glVertexAttribPointer
 
   bool loadGLFunctions();
 #endif
